e146801fa0df8bd114587ae8e3625184f5a23d44
Quy Nguyen <nguyenbaquy93@gmail.com>
2023-01-12 03:18:25 +0700

    Add host inducate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
 const start = async () => {
-  app.listen(3000, () => {
+  app.listen(3000, "0.0.0.0", () => {
     console.log("Listening on port 3000!!!!!!!!");
   });
 };
